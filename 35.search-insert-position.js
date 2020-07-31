63bcf91802f8074c268bda280311c9973f718c7d
jinling <jinling.sunshine@gmail.com>
2020-07-31 14:45:52 +0800

    [35] optimize my solution according to solution on the internet

--- 35.search-insert-position.js
+++ 35.search-insert-position.js
@@ -11,74 +11,43 @@
  * @return {number}
  */
 // 考点应该是二分查找
+
 var searchInsert = function (nums, target) {
-  let start = 0,
-    end = nums.length - 1;
-  let mid = start + index(end - start);
-  // 数组len=1
-  if (start == end) {
-    if (target > nums[end])
-      return 1
-    else if (target < nums[start])
-      return 0
-    else
-      return start
-  } else if (end - start == 1) {
-    if (target > nums[end])
-      return end + 1
-    else if (target < nums[start])
-      return 0
-    else if (target == nums[start])
-      return start
-    else
-      return end
-  } else {
-    while (end - start > 1) {
-      if (target < nums[mid]) {
-        console.log('target:' + target + '<' + "nums[" + mid + "]:" + nums[mid]);
-        end = mid;
-        if (end - start == 1) {
-          if (target == nums[start]) {
-            return start
-          } else if (target < nums[start]) {
-            return 0
-          } else {
-            return start + 1
-          }
-        } else {
-          mid = start + index(end - start);
-        }
-      } else if (target > nums[mid]) {
-        console.log('target:' + target + '>' + "nums[" + mid + "]:" + nums[mid]);
-        start = mid;
-        if (end - start == 1) {
-          if (target == nums[end]) {
-            return end
-          } else if (target > nums[end]) {
-            return end + 1
-          } else {
-            return start + 1
-          }
-        } else {
-          mid = start + index(end - start);
-        }
-      } else {
-        // console.log(mid);
-        return mid;
-      }
+
+  if (nums.length == 0)
+    return 0
+  let start = 0, end = nums.length - 1, mid = 0;
+  if (target > nums[end])
+    return end + 1;
+  // if (target < nums[start])
+  //   return 0;
+  // if (target == nums[start])
+  //   return start
+
+  while (end > start) {
+    mid = start + parseInt((end - start) / 2); // parseInt 直接丢弃小数部分, 保留整数部分
+    console.log('mid: ', mid, ' start: ', start, ' end: ', end);
+    if (target == nums[start])
+      return start;
+    if (target == nums[end])
+      return end;
+    if (target == nums[mid])
+      return mid;
+
+    if (target > nums[mid]) {
+      start = mid + 1;
+      // console.log('target > mid', 'start: ', start);
+    } else {
+      end = mid;
+      // console.log('target < mid', 'end: ', end);
     }
   }
-};
 
-var index = (gap) => {
-  if (gap % 2 == 0)
-    return gap / 2;
-  else
-    return (gap + 1) / 2
+  return start
 }
 
-let test = [1, 3];
-let res = searchInsert(test, 4);
+let test = [1, 3, 5, 6, 8, 10, 12, 14, 16];
+let res = searchInsert(test, 2);
 console.log(res);
 // @lc code=end
 
