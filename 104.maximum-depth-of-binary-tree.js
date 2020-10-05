c6092d5cd1ecb76d994fb610aade5f5170f2f2a6
Gillian97 <jinling.sunshine@gmail.com>
2020-10-05 21:11:59 +0800

    add 104

---
+++ 104.maximum-depth-of-binary-tree.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=104 lang=javascript
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+/* 递归法 */
+// 主要是需要进行高度之间的比较
+// 不要共用一个高度值 要直接返回数字 进行数字之间的相加
+// 与数二叉树的节点类似
+var maxDepth = function (root) {
+  return helper(root, 0);
+};
+
+var helper = (root, depth) => {
+  // 根节点为空 直接返回上一层高度即可
+  if (!root) return depth;
+  if (!root.left && !root.right) { // 叶子节点
+    return depth + 1;
+  } else {
+    return Math.max(helper(root.left, depth + 1), helper(root.right, depth + 1));
+  }
+}
+// @lc code=end
+
