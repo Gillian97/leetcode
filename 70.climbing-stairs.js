226c41eeaa8eb0a34047ffb041b5bf457a3f1ad1
Gillian97 <jinling.sunshine@gmail.com>
2020-11-14 18:10:51 +0800

    update 70

--- 70.climbing-stairs.js
+++ 70.climbing-stairs.js
@@ -13,7 +13,9 @@
 // 需要返回达到n层总共的路径数
 // 到达每一层都有两种方式, 要不跨一步, 要不跨两步
 // 假设到达i层的方法数为dp[i], 则dp[i] = dp[i-1] + dp[i-2]
-var climbStairs = function (n) {
+
+// solution 1: 使用 dp 数组进行迭代 自底向上
+var climbStairs1 = function (n) {
   let way = new Array(n);
   way.fill(0);
   way[0] = 1;
@@ -23,5 +25,20 @@ var climbStairs = function (n) {
   }
   return way[n - 1];
 };
+
+// solution 2: 使用备忘录优化重叠子问题 自顶向下
+var climbStairs = function (n) {
+  let note = {};
+  return helper(n, note);
+};
+
+var helper = (n, note) => {
+  if (n == 1 || n == 2) return n;
+  if (note.hasOwnProperty(n)) return note[n];
+  // calculate
+  let res = helper(n - 1, note) + helper(n - 2, note);
+  note[n] = res;
+  return res;
+}
 // @lc code=end
 
