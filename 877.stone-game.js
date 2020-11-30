e5245c78d8feb12579e4b7d0ee841a9da68f7235
Gillian97 <jinling.sunshine@gmail.com>
2020-11-30 21:11:18 +0800

    add 877

---
+++ 877.stone-game.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=877 lang=javascript
+ *
+ * [877] Stone Game
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+// 将石子分为两组之后, 可以知道哪一组的石子更多.
+// 作为先手可以全程只取某一组的石子, 因此在没有平局的情况下
+// 可以有必胜策略, 因此先手总是可以赢得比赛.
+var stoneGame = function (piles) {
+  return true;
+};
+// @lc code=end
+
